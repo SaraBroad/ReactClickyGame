b3e76d67f1bde2790c929ff751f871cfab32b459
Sara Broad <saraebroad@gmail.com>
2018-06-16 18:12:21 -0400

    NavBar code

---
+++ clickygame/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+// const NavBar = props => <Nav className="nav">{props.children}</header>
+
+const NavBar = props => (
+
+<nav className="nav">
+
+
+</nav>
+
+);
+
+export default NavBar;
