43c1e8b6cc51c3799e58bbcd7c7db0ba11535701
Sara Broad <saraebroad@gmail.com>
2018-06-16 17:33:36 -0400

    changing app.js

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 
 class App extends Component {
 
-  state = {
-    artists
-  };
+  // state = {
+  //   artists
+  // };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+   
     );
   }
 }
