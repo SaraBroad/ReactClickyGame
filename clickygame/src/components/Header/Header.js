97ce3def2e5a219dc274f982aea6a9cb825c735a
Sara Broad <saraebroad@gmail.com>
2018-06-16 18:09:39 -0400

    NavBar files

--- clickygame/src/components/Header/Header.js
+++ clickygame/src/components/Header/Header.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Header.css";
 
-const Header = props => <header className="header">{[props.children]}</header>
+// const Header = props => <header className="header">{props.children}</header>
+
+const Header = props => (
+
+<header className="header">
+
+
+</header>
+
+);
 
 export default Header;
